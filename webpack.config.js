958a462aac111ebfadb975178dc3b878eded791c
varun.vc <Varun.VC@onsolve.com>
2024-04-09 12:10:12 +0530

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.join(__dirname, "src", "index.js"),
+    output: {
+        path: path.resolve(__dirname, "dist"),
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        alias: {
+            'components': path.resolve(__dirname, 'src/Components')
+        }
+    },
+    devServer: {
+        host: 'localhost',
+        port: 3000,
+        historyApiFallback: true,
+        open: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.svg$/,
+                use: ['@svgr/webpack'],
+            },
+            {
+                test: /\.(png|jp(e*)g|svg|gif)$/,
+                use: ['file-loader'],
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.?(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader",
+                  options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                  }
+                }
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: path.join(__dirname, "src", "index.html"),
+        }),
+    ]
+}
